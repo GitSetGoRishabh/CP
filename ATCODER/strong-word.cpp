f6ce770ce32272b58c0a046c1a6c8b9385449254
Rishabh Singh Yadav <rishabhgoterror@gmail.com>
2026-02-14 18:13:00 +0530

    atcoder weekly 444 q1 strong word

---
+++ ATCODER/strong-word.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin>>s;
+    int n=s.size();
+    int count=0;
+    if(s[0]==s[n-1]) cout<<"Yes"<<endl;
+    else cout<<"No"<<endl;
+}
